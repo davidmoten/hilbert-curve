e75174099d806e04f1e41581ccc60ff2fe31d87c
Dave Moten <davidmoten@gmail.com>
2017-02-23 19:53:00 +1100

    add benchmark

---
+++ src/test/java/org/davidmoten/hilbert/Benchmarks.java
@@ -0,0 +1,20 @@
+package org.davidmoten.hilbert;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.infra.Blackhole;
+
+public class Benchmarks {
+    
+    private static final int BITS = 10;
+    private static final long N = Math.round(Math.pow(2, BITS)) - 1;
+    private static final HilbertCurve c = HilbertCurve.bits(BITS).dimensions(5);
+    
+    @Benchmark
+    public void roundTrip(Blackhole b) {
+        for (long i = 0;i<N; i++) {
+            long[] point = c.point(i);
+            b.consume(i == c.index(point).longValue());
+        }
+    }
+
+}
