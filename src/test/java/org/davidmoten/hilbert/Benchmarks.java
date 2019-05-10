f32ec36e70f3b7a82f082c7a80c8edfd2dd92456
David Moten <davidmoten@gmail.com>
2019-05-10 16:45:22 +1000

    modify Benchmark names and fix using all points

--- src/test/java/org/davidmoten/hilbert/Benchmarks.java
+++ src/test/java/org/davidmoten/hilbert/Benchmarks.java
@@ -10,15 +10,15 @@ import org.openjdk.jmh.infra.Blackhole;
 public class Benchmarks {
 
     private static final int BITS = 10;
-    private static final long N = 1L << BITS - 1;
     private static final int DIMENSIONS = 5;
     private static final HilbertCurve c = HilbertCurve.bits(BITS).dimensions(DIMENSIONS);
     private static final SmallHilbertCurve small = HilbertCurve.small().bits(BITS).dimensions(DIMENSIONS);
+    private static final int N = (int) small.maxOrdinate();
     private static final long[] point = new long[DIMENSIONS];
     private static final List<long[]> points = createPoints();
 
     @Benchmark
-    public void roundTripTimes512(Blackhole b) {
+    public void roundTripAllPoints10Bits1024Calls(Blackhole b) {
         for (long i = 0; i < N; i++) {
             long[] point = c.point(i);
             b.consume(c.index(point).longValue());
@@ -26,28 +26,28 @@ public class Benchmarks {
     }
 
     @Benchmark
-    public void toIndexTimes512(Blackhole b) {
+    public void toIndexAllPoints10Bits1024Calls(Blackhole b) {
         for (int i = 0; i < N; i++) {
             b.consume(c.index(points.get(i)).longValue());
         }
     }
 
     @Benchmark
-    public void toIndexTimes512Small(Blackhole b) {
+    public void toIndexAllPoints10Bits1024CallsSmall(Blackhole b) {
         for (int i = 0; i < N; i++) {
             b.consume(small.index(points.get(i)));
         }
     }
 
     @Benchmark
-    public void pointTimes512(Blackhole b) {
+    public void pointAllPoints10Bits1024Calls(Blackhole b) {
         for (long i = 0; i < N; i++) {
             b.consume(c.point(i));
         }
     }
 
     @Benchmark
-    public void roundTripSmallTimes512(Blackhole b) {
+    public void roundTripSmallAllPoints10Bits1024Calls(Blackhole b) {
         for (long i = 0; i < N; i++) {
             long[] point = small.point(i);
             b.consume(small.index(point));
@@ -55,14 +55,14 @@ public class Benchmarks {
     }
 
     @Benchmark
-    public void pointSmallTimes512(Blackhole b) {
+    public void pointSmallAllPoints10Bits1024Calls(Blackhole b) {
         for (long i = 0; i < N; i++) {
             b.consume(small.point(i));
         }
     }
 
     @Benchmark
-    public void roundTripTimes512LowAllocation(Blackhole b) {
+    public void roundTripAllPoints10Bits1024CallsLowAllocation(Blackhole b) {
         for (long i = 0; i < N; i++) {
             c.point(i, point);
             b.consume(c.index(point).longValue());
@@ -70,7 +70,7 @@ public class Benchmarks {
     }
 
     @Benchmark
-    public void pointTimes512LowAllocation(Blackhole b) {
+    public void pointAllPoints10Bits1024CallsLowAllocation(Blackhole b) {
         for (long i = 0; i < N; i++) {
             c.point(i, point);
             b.consume(point);
@@ -78,7 +78,7 @@ public class Benchmarks {
     }
 
     @Benchmark
-    public void roundTripSmallTimes512LowAllocation(Blackhole b) {
+    public void roundTripSmallAllPoints10Bits1024CallsLowAllocation(Blackhole b) {
         for (long i = 0; i < N; i++) {
             small.point(i, point);
             b.consume(small.index(point));
@@ -86,7 +86,7 @@ public class Benchmarks {
     }
 
     @Benchmark
-    public void pointSmallTimes512LowAllocation(Blackhole b) {
+    public void pointSmallAllPoints10Bits1024CallsLowAllocation(Blackhole b) {
         for (long i = 0; i < N; i++) {
             small.point(i, point);
             b.consume(point);
